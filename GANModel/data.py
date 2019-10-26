60216c6e9ae2aa0ac4eed1ecb2ab32bc3e493587
Jack Tolley <JackRTolley@gmail.com>
2019-10-26 20:10:29 +0100

    Moved Model into folder

--- GANModel/data.py
+++ GANModel/data.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
-data = pd.read_json("Feb28.json", lines=True, chunksize=100000)
+
+import torch.nn as nn
+
+data = pd.read_json("./Feb28.json", lines=True, chunksize=100000)
+
+
+def formati(x):
+
+    r = [a for a in x.lower() if a in 'abcdefghijklmnopqrstuvwxyz,. !?&']
+    r = "".join(r)
+    return r
+
 
 limit = 0
 for chunk in data:
@@ -8,9 +19,12 @@ for chunk in data:
     block = pd.DataFrame(chunk)
     block = block[['controversiality','body', 'score', 'gildings']]
     block = block[block['controversiality']==1]
+    block = block[block['body'].apply(len) <= 250]
+    block = block['body'].apply(formati)
 
     #popopopop
     with open('Feb28Filtered.csv', 'a') as f:
         block.to_csv(f, header=False)
 
     print(block)
+
