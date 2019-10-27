cc6eac1e032bf0f2c7721ceef0c3a88049463ca5
Musab Gumaa <musabgumaa@gmail.com>
2019-10-27 06:06:06 +0000

    Updated Flask API to allow for CORS requests

--- MamboViperBack/app.py
+++ MamboViperBack/app.py
@@ -3,12 +3,21 @@ from flask import Flask, request
 from flask import jsonify
 from main import *
 
+from twilio.rest import Client
+from twilio import twiml
+
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
+@cross_origin
 def index():
     subreddit = request.args.get('subreddit')
     return jsonify(CommentGenerator().run(subreddit))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
