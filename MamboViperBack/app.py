30f26b56b7b770d139cef0516aed6b0f3e82f9a6
Musab Gumaa <musabgumaa@gmail.com>
2019-10-27 07:07:09 +0000

    Added twilio test function

--- MamboViperBack/app.py
+++ MamboViperBack/app.py
@@ -17,6 +17,26 @@ def index():
     subreddit = request.args.get('subreddit')
     return jsonify(CommentGenerator().run(subreddit))
 
+account_sid = "AC2470d0b52f42a90745b77bec451b7ec7"
+auth_token = "dbf82fb8f77da28e5c4d03edc2756a9e"
+
+@app.route('/smsService')
+@cross_origin()
+def smsService():
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                .create(
+                    body="An Internet Game for Horrible Devs",
+                    from_="+18152614761",
+                    to="+447392507342"
+                )
+
+    return jsonify({
+        "content": "An Internet Game for Horrible Devs",
+    })
+    
+
 
 
 if __name__ == '__main__':
