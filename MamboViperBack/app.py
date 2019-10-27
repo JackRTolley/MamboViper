6b5cca567dfdf511827a3db6e6b0aedf8e2e5685
Musab Gumaa <musabgumaa@gmail.com>
2019-10-27 04:25:00 +0000

    Improved Request Formatting

--- MamboViperBack/app.py
+++ MamboViperBack/app.py
@@ -1,12 +1,13 @@
 #!flask/bin/python
 from flask import Flask
+from flask import jsonify
 from main import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return CommentGenerator().run()
+    return jsonify(CommentGenerator().run())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- MamboViperBack/main.py
+++ MamboViperBack/main.py
@@ -4,9 +4,8 @@ class CommentGenerator:
     def __init__(self):
         pass
 
-    def validateCommandLineArguments(self):
+    def validateCommandLineArguments(self, args):
         try:
-            args = sys.argv
             # print(args)
             if ( "-a" in args or "--All" in args ):
                 # Everything
@@ -25,28 +24,38 @@ class CommentGenerator:
             elif  ( "-h" in args or "--Help" in args):
                 return ( "Help Menu..." + self.displayCommandHelp())
             else:
-                raise Exception(("Invalid Option.\nPlease see the below help section."))
+                raise Exception(("Invalid Paramater."))
+
+            return({
+                "result": "success",
+                "info": "Successfully Generated Comments",
+                "comments": "here",
+            })
         except Exception as e:
             # stdout command help.
-            return (f"An Error Occured: {e}" + self.displayCommandHelp())
+            print(self.displayCommandHelp())
+            return ({
+                "result": "error",
+                "info": "An Error occurred whilst generating comments.",
+                "error":f"{e}"
+                })
          
     
     def displayCommandHelp(self):
-        return ("""
-        
-        Options
-        ----------------------
-        -s, --Sport         Provide only Sports-related comments
-        -g, --Gaming        Provide only Gaming-related comments
-        -f, --Funspiracy    Provide only Fun-conspiracy-related commments
-        -r, --Reddit        Provide only General Reddit Comments (i.e. AskReddit)
+        return ("""        
+Options
+----------------------
+-s, --Sport         Provide only Sports-related comments
+-g, --Gaming        Provide only Gaming-related comments
+-f, --Funspiracy    Provide only Fun-conspiracy-related commments
+-r, --Reddit        Provide only General Reddit Comments (i.e. AskReddit)
 
-        -a, --All           Provide comments from all above categories.
+-a, --All           Provide comments from all above categories.
 
-        Miscellaneous
-        ----------------------
-        -h, --Help          Display this help menu.
-        \n
+Miscellaneous
+----------------------
+-h, --Help          Display this help menu.
+\n
         """)
 
 
